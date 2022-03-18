8cc43c24abc945ab6e66ce95b67196c3f977e909
Sathvikreddy14 <98522106+Sathvikreddy14@users.noreply.github.com>
2022-03-18 11:50:07 +0530

    Create BinaryToDecimal.go

---
+++ BinaryToDecimal.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var num int
+	fmt.Print("enter no")
+	fmt.Scanln(&num)
+	fmt.Println(BinaryToDecimal(num))
+
+}
+
+func BinaryToDecimal(num int) int {
+	var remainder int
+	var i float64
+	var decimal int
+	for i = 0; num != 0; i++ {
+		remainder = num % 10
+		num = num / 10
+		decimal = decimal + remainder*int(math.Pow(2, i))
+
+	}
+	return decimal
+}
